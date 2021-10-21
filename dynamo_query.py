8f5867bf6d3a0fe7a0989765d2ec18178a2d0dc9
Darek <dkrysmann@gmail.com>
2021-10-21 14:43:17 +0000

    dynamo query

---
+++ dynamo_query.py
@@ -0,0 +1,29 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+
+def query_dynamo_db(search_string, table_name, key, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table(table_name)
+    response = table.query(
+        KeyConditionExpression=Key(key).eq(search_string)
+    )
+
+    return response['Items']
+
+def put_flight(*args, table_name, dynamodb=None):
+    #     print('ARGS:    ', args[0])
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb') 
+
+    table = dynamodb.Table(table_name)
+    response = table.put_item(
+       Item=args[0]
+    )
+
+    return response
+
+result = query_dynamo_db('the search key', 'table_name', 'Partition key name')
+
+result = put_flight('json string having partition_key and sort key names', 'table_name')

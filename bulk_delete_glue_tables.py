e1867905607f6ce3b0c3697badad794f1369ec7d
Darek <dkrysmann@gmail.com>
2021-10-21 16:42:19 +0000

    AWS Glue list and delete tables (Athena)

--- bulk_delete_glue_tables.py
+++ bulk_delete_glue_tables.py
@@ -1,21 +1,28 @@
 import boto3
 
+'''Set client and region'''
 glue_client = boto3.client('glue', region_name='eu-west-1')
 
-response = glue_client.get_tables(
-    # CatalogId='string',
-    DatabaseName='piaware',
-    # Expression='string',
-    # NextToken='string',
-    MaxResults=200
-)
+'''Standard boto3 documentation description'''
+# response = glue_client.get_tables(
+#     CatalogId='string',
+#     DatabaseName='piaware',
+#     Expression='string',
+#     NextToken='string',
+#     MaxResults=100
+# )
 
+'''Using NextToken in boto3 output to create list of tables in database piaware'''
 results = response["TableList"]
 while "NextToken" in response:
-    response = glue_client.get_tables(NextToken=response["NextToken"], DatabaseName='piaware')
+    response = glue_client.get_tables(
+        NextToken=response["NextToken"], 
+        DatabaseName='piaware'
+        )
     results.extend(response["TableList"])
     
 
+'''Delete all tables having 'df_parquet_gzip' in name'''
 i = 1
 cnt = len(results)
 for tbl in results:

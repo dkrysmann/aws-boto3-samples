3836191d6581fef18317bf64e0b6b10cd0b2a55a
Darek <dkrysmann@gmail.com>
2021-10-22 12:06:17 +0000

    Read s3 from lambda

--- read_s3_content.py
+++ read_s3_content.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
     Read s3 object content for example access s3 object content in lambda function
 '''
 
-uri = 's3://aws-athena-query-results-000290283155-eu-central-1/lambda/c5b49b66-c034-4830-a3a0-e5fac5dd4650.csv'
+uri = 's3://aws-athena-query-results-0000000-eu-central-1/lambda/c5b49b66-c034-4830-a3a0-e5fac5dd4650.csv'
 path = urlparse(uri)
 print(path[2].strip('/'))
 
